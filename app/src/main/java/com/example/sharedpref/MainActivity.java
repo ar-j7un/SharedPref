7d930c5c14b23a61377ee7e51c8806f27c502680
CCLAB15 <CCLAB15@ccl15.rcss.com>
2023-09-04 09:29:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sharedpref/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.sharedpref;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.sax.StartElementListener;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText ed1,ed2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ed1=(EditText) findViewById(R.id.e1);
+        ed2=(EditText) findViewById(R.id.e2);
+    }
+    public void onLogin(View v)
+    {
+
+        SharedPreferences sp;
+        sp=getSharedPreferences("SD", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor ed=sp.edit();
+        ed.putString("un",ed1.getText().toString());
+        ed.putString("up",ed2.getText().toString());
+
+        ed.commit();
+        Intent in = new Intent(this, Second.class);
+        startActivity(in);
+
+
+
+    }
+
+}
